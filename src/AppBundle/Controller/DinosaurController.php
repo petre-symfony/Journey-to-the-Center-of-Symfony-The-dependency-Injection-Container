3f63aea3f489d4eefeab20ca28c9087f9fcb75cc
petrero <petrero@gmail.com>
2017-05-29 17:29:49 +0300

    Copy code from Knp site. Run:   composer install   composer update   php app/console doctrine:database:create   php app/console doctrine:schema:create   php app/console doctrine:fixtures:load

---
+++ src/AppBundle/Controller/DinosaurController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Dinosaur;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class DinosaurController extends Controller {
+  /**
+   * @Route("/", name="dinosaur_list")
+   */
+  public function indexAction($isLinux){
+    $dinos = $this->getDoctrine()
+      ->getRepository('AppBundle:Dinosaur')
+      ->findAll();
+
+    /*
+    $request = new Request();
+    $request->attributes->set(
+      '_controller',
+      'AppBundle:Dinosaur:_latestTweets'
+    );
+    $httpKernel = $this->container->get('http_kernel');
+    $response = $httpKernel->handle(
+      $request,
+      HttpKernelInterface::SUB_REQUEST
+    );
+    */
+
+    return $this->render('dinosaurs/index.html.twig', [
+      'dinos' => $dinos,
+      'isLinux' => $isLinux
+    ]);
+  }
+
+  /**
+   * @Route("/dinosaurs/{id}", name="dinosaur_show")
+   */
+  public function showAction($id){
+    $dino = $this->getDoctrine()
+      ->getRepository('AppBundle:Dinosaur')
+      ->find($id);
+
+    if (!$dino) {
+      throw $this->createNotFoundException('That dino is extinct!');
+    }
+
+    return $this->render('dinosaurs/show.html.twig', [
+      'dino' => $dino,
+    ]);
+  }
+
+  public function _latestTweetsAction($userOnLinux){
+    $tweets = [
+      'Dinosaurs can have existential crises too you know.',
+      'Eating lollipops... ',
+      'Rock climbing... '
+    ];
+
+    return $this->render('dinosaurs/_latestTweets.html.twig', [
+      'tweets' => $tweets,
+      'isLinux' => $userOnLinux
+    ]);
+  }
+}

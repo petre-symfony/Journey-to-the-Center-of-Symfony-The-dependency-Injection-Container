02a0f9410b0accb36a2b26436865a0ca5bb81ef3
petrero <petrero@gmail.com>
2017-06-01 14:01:13 +0300

    After a container is built, you should compile it.
    This starts one final layer to the build process, which anyone can hook into to make final adjustments. For now, it's not doing anything - but it's really important inside the framework.
    
    In a big project - parsing Yaml files and collecting all this service Definition stuff can start to take a lot of time. Our container is nice, but it's coming at a performance cost.
    
    Let's see how much by adding some really basic profiling code. Up top, add a {dollar_sign}startTime variable. And down below, figure out how much time elapsed, multiply it by 1000 to get microseconds, and while we're here, round it. And hey, let's use our container to get out the logger and debug a message about this.
    
    So let's see how long this takes:
    
    php dino_container/roar.php
    37ms at first, but then it settles to about 19ms after running a few times. Not bad, but this is a tiny project. Just keep that 19ms number in mind.

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -8,13 +8,20 @@ use Symfony\Component\Config\FileLocator;
 
 require __DIR__.'/../vendor/autoload.php';
 
+$start = microtime(true);
+
 $container = new ContainerBuilder();
 $container->setParameter('root_dir', __DIR__);
 
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
 $loader->load('services.yml');
 
+$container->compile();
+
 runApp($container);
+
+$elapsed = round((microtime(true) - $start) * 1000);
+$container->get('logger')->debug('Elapsed Time: '.$elapsed.'ms');
 
 function runApp(ContainerBuilder $container){
   $container->get('logger')->info('ROOOOAR'); 

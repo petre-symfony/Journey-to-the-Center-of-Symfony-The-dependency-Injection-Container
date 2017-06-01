12e16fff723d763313957813c15f1f7987647184
petrero <petrero@gmail.com>
2017-06-01 13:20:01 +0300

    Copy code from branch 4

---
+++ dino_container/roar.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dino\Play;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$container = new ContainerBuilder();
+
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
+$loader->load('services.yml');
+
+$handlerDefinition = new Definition('Monolog\Handler\StreamHandler');
+$handlerDefinition->setArguments(array(__DIR__.'/dino.log'));
+$container->setDefinition('logger.stream.handler', $handlerDefinition);
+
+$stdLoggerDefinition = new Definition('Monolog\Handler\StreamHandler');
+$stdLoggerDefinition->setArguments(array(
+  'php://stdout'
+));
+$container->setDefinition('logger.std_out_logger', $stdLoggerDefinition);
+
+runApp($container);
+
+function runApp(ContainerBuilder $container){
+  $container->get('logger')->info('ROOOOAR'); 
+}
+

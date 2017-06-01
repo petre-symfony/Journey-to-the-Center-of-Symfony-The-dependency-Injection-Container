ae96481f28f2d951ffd9ee15b6bad7198ca965c9
petrero <petrero@gmail.com>
2017-06-01 13:40:51 +0300

    To use a parameter in Yaml, say %root_dir%.
    With everything in Yaml, we can clean up! We don't need any Definition code at all in roar.php - just create the container, set the parameter and load the yaml file.
    
    Ok, moment of truth!
    
    php dino_container/roar.php
    tail dino_container/dino.log
    It still prints! And it's still adding to our log file. And now all that service Definition code is sitting in services.yml.

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -3,8 +3,6 @@
 namespace Dino\Play;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
@@ -15,11 +13,6 @@ $container->setParameter('root_dir', __DIR__);
 
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
 $loader->load('services.yml');
-
-$handlerDefinition = new Definition('Monolog\Handler\StreamHandler');
-$handlerDefinition->setArguments(array(__DIR__.'/dino.log'));
-$container->setDefinition('logger.stream.handler', $handlerDefinition);
-
 
 runApp($container);
 

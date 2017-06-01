1919276209342b4cb2bcd5e2b70a398f97e0746b
petrero <petrero@gmail.com>
2017-06-01 12:04:13 +0300

    Copy code from branch 2

---
+++ dino_container/roar.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dino\Play;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$container = new ContainerBuilder();
+
+$loggerDefinition = new Definition('Monolog\Logger');
+$loggerDefinition->setArguments(array(
+  'main',
+  array(new Reference('logger.stream.handler'))
+));
+$container->setDefinition('logger', $loggerDefinition);
+
+$handlerDefinition = new Definition('Monolog\Handler\StreamHandler');
+$handlerDefinition->setArguments(array(__DIR__.'/dino.log'));
+$container->setDefinition('logger.stream.handler', $handlerDefinition);
+
+runApp($container);
+
+function runApp(ContainerBuilder $container){
+  $container->get('logger')->info('ROOOOAR'); 
+}
+
